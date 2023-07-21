70e30e337b734ed1d51ef26141eaef9f59450eeb
fjfj10 <kimmisuk2218@gmail.com>
2023-07-21 11:09:04 +0900

    comment: 주석으로 답 추가

---
+++ dml/unicef example.sql
@@ -0,0 +1,103 @@
+# 1. 각 계정별로 어떤 기부를 했는지
+select
+	gt.giver_id,
+    gpt.giving_name,
+    gt.giving_total
+from
+	giver_tb gt 
+    left outer join giving_page_tb gpt on(gpt.giving_page_id = gt.giving_page_id)
+where
+	gpt.giving_name != 'test';	-- giving_name이 'test'인 것을 제외
+-- 답
+-- select
+-- 	gt.giver_id,
+-- 	gt.user_id,
+-- 	ut.name,
+--  gt.giving_page_id,
+--  gpt.story_title,
+--  gt.giving_total
+-- from
+-- 	giver_tb gt 
+--  left outer join user_tb ut on(ut.user_id = gt.user_id)
+--  left outer join giving_page_tb gpt on(gpt.giving_page_id = gt.giving_page_id)     
+-- group by
+-- 	gt.user_id;
+
+# 2. 센터별로 총 기부액이 얼마인지
+select
+	ct.center_id,
+	ct.center_name,
+    ifnull(sum(gt.giving_total), 0) as center_giving_total
+from
+	center_tb ct
+    left outer join giving_page_tb gpt on(gpt.center_id = ct.center_id)
+    left outer join giver_tb gt on(gt.giving_page_id = gpt.giving_page_id)
+where
+	ct.center_name != 'test'	-- center_name이 'test'인 것을 제외
+group by 
+	ct.center_id;
+
+-- 답
+-- select
+-- 	ct.center_id,
+-- 	ct.center_name,
+-- 	ct.center_address,
+--  sum(gvt.giving_total) as total_giving_money
+-- from
+-- 	center_tb ct
+--  left outer join giving_page_tb gpt on(gpt.center_id = ct.center_id)
+--  left outer join giver_tb gvt on(gvt.giving_page_id = gpt.giving_page_id)
+-- group by 
+-- 	ct.center_id,
+-- 	ct.center_name,
+--  ct.center_address;
+
+
+# 3. 기부 항목별 총 기부액
+select 
+	gpt.giving_name,
+    ifnull(sum(gt.giving_total), 0) as giving_name_total
+ from
+ 	giving_page_tb gpt
+ 	left outer join giver_tb gt on(gt.giving_page_id = gpt.giving_page_id)
+where
+	gpt.giving_name != 'test'	-- giving_name이 'test'인 것을 제외
+group by
+    gpt.giving_name;
+
+-- 답
+-- select 
+-- 	gpt.giving_page_id,
+-- 	gpt.giving_name,
+-- 	ifnull(sum(gvt.giving_total), 0) as total_giving_money
+-- from
+-- 	giving_page_tb gpt
+-- 	left outer join giver_tb gvt  on(gvt.giving_page_id = gpt.giving_page_id)
+-- group by
+-- 	gpt.giving_page_id,
+-- 	gpt.giving_name;
+    
+    
+# 4. category별 총 기부액
+select
+	gct.giving_category_id,
+	gct.giving_category_name,
+    sum(gt.giving_total) as giving_category_total
+from
+	giving_category_tb gct
+    left outer join giving_page_tb gpt on(gpt.category_id = gct.giving_category_id)
+    left outer join giver_tb gt on(gt.giving_page_id = gpt.giving_page_id)
+group by
+	gct.giving_category_id,
+	gct.giving_category_name;
+    
+-- 답
+-- select
+-- 	gct.giving_category_name,
+--     ifnull(sum(gvt.giving_total), 0) as total_giving_money
+-- from
+-- 	giving_category_tb gct
+--     left outer join giving_page_tb gpt on(gpt.category_id = gct.giving_category_id)
+--     left outer join giver_tb gvt on(gvt.giving_page_id = gpt.giving_page_id)
+-- group by
+-- 	gct.giving_category_name;
